f7412ae964a9e9aa2b0e267ce515cebf44dbd714
Tomasz Sosnowski <tomek@Mac-Pro.local>
2018-03-26 00:51:22 +0200

    Update to 0.5.9b. Right version number

--- src/Koralfx.cpp
+++ src/Koralfx.cpp
@@ -13,7 +13,7 @@ void init(rack::Plugin *p) {
 	//p->slug = TOSTRING(SLUG);
 	//p->version = TOSTRING(VERSION);
 	p->slug = "KoralfxVCV";
-	p->version = "0.5.9a";	
+	p->version = "0.5.9b";	
 	p->website = "https://github.com/koralfx/KoralfxVCV";
 	p->manual = "https://github.com/koralfx/KoralfxVCV/blob/master/README.md";
 
